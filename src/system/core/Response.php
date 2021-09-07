a4352a16f84043005efd28045e03f8a776cc7073
KsoftM <90229823+KsoftM@users.noreply.github.com>
2021-09-07 20:12:56 +0530

    Update Response.php

--- src/system/core/Response.php
+++ src/system/core/Response.php
@@ -5,20 +5,7 @@ namespace ksoftm\system\core;
 
 class Response
 {
-    // return response($content)->header('Content-Type', $type)
-    // ->cookie('name', 'value', $minutes);
-
-    // return response()->download($pathToFile, $name, $headers);
-
-    // return response()->json($data, 200, $headers);
-
-    // return Response::make($contents, 200, $headers);
-
-    // return response($content)->withHeaders([
-    //     'Content-Type' => $type,
-    //     'X-Header-One' => 'Header Value',
-    // ]);
-
+    
     public static function make(string $contents, int $responseCode, array $headers): Response
     {
         # code...
