d9d0ffb995b22b4cc71d23f684aae1662d62fcf4
KsoftM <ksoftmanagement@gmail.com>
2021-10-25 13:06:43 +0530

    bug fixed

--- src/helper.php
+++ src/helper.php
@@ -18,21 +18,3 @@ if (!function_exists('redirect')) {
         Response::make()->header("Location: $path");
     }
 }
-
-
-if (!function_exists('request_dir')) {
-    function request_dir(string $dir): array
-    {
-        $f = new FileManager($dir);
-        $output = [];
-        if ($f->isExist()) {
-            $f = $f->getDirectoryFiles(true);
-            foreach ($f as $data) {
-                if ($data instanceof FileManager) {
-                    $output[] = $data->requireOnce();
-                }
-            }
-        }
-        return $output;
-    }
-}
